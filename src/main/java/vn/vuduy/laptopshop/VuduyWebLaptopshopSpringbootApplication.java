fe5ba015f56a1444efa115efa352c6e6f6a47ff7
vuduy24122004 <vuduy24122004@gmail.com>
2025-04-23 02:28:40 +0700

    Disable Security

--- src/main/java/vn/vuduy/laptopshop/VuduyWebLaptopshopSpringbootApplication.java
+++ src/main/java/vn/vuduy/laptopshop/VuduyWebLaptopshopSpringbootApplication.java
@@ -3,7 +3,10 @@ package vn.vuduy.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+
+//Disable Security(Bảo mật login)
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class VuduyWebLaptopshopSpringbootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/vn/vuduy/laptopshop/controller/UserControler.java
@@ -0,0 +1,8 @@
+package vn.vuduy.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserControler {
+
+}
