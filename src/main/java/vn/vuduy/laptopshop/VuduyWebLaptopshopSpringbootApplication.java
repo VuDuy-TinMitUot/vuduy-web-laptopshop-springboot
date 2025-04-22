421884f4ca8c3f9e7c9a14dbf9492ff6b3a11592
vuduy24122004 <vuduy24122004@gmail.com>
2025-04-23 01:12:26 +0700

    Inital project initiation

---
+++ src/main/java/vn/vuduy/laptopshop/VuduyWebLaptopshopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package vn.vuduy.vuduy_web_laptopshop_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuduyWebLaptopshopSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuduyWebLaptopshopSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/vuduy/vuduy_web_laptopshop_springboot/VuduyWebLaptopshopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.vuduy.vuduy_web_laptopshop_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuduyWebLaptopshopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
